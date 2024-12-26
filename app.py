41889fa1a94bfdb8f49c3d8f3ebaf5741810a9d8
Daniel Coblentz <danielcoblentz916@gmail.com>
2024-12-25 19:42:35 -0500

    testing feature

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your_secret_key'
-app.config['SQLALCHEMY_DATABASE_URI'] = None
+app.config['SQLALCHEMY_DATABASE_URI'] = one
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = False
 
